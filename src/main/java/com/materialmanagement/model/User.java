fc726f456935ae2f1de142cb4fa64a21d8b1a2ec
CamiloCstro <camilocstro@gmail.com>
2021-11-01 21:43:10 -0500

    model User table

---
+++ src/main/java/com/materialmanagement/model/User.java
@@ -0,0 +1,34 @@
+package com.materialmanagement.model;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    private Integer id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String last_name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private boolean active;
+
+}
