ad2e44d763fef5b8589ec11ba8b7c206ed7d77d7
CamiloCstro <camilocstro@gmail.com>
2021-11-01 22:33:04 -0500

    create jpa repository to User model controller

---
+++ src/main/java/com/materialmanagement/repository/IUserRepository.java
@@ -0,0 +1,9 @@
+package com.materialmanagement.repository;
+
+import com.materialmanagement.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, Integer> {
+}
