cc7feefb0d53a1f0733c94d134a33c74d4cf1e58
CamiloCstro <camilocstro@gmail.com>
2021-11-02 01:01:41 -0500

    Locality Controller CRUD

---
+++ src/main/java/com/materialmanagement/controller/LocalityController.java
@@ -0,0 +1,75 @@
+package com.materialmanagement.controller;
+
+import com.materialmanagement.model.Locality;
+import com.materialmanagement.model.User;
+import com.materialmanagement.repository.ILocalityRepository;
+import com.materialmanagement.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/Locality")
+public class LocalityController {
+
+    @Autowired
+    ILocalityRepository localityRepository;
+    @Autowired
+    IUserRepository userRepository;
+
+    @GetMapping("/locality")
+    public List<Locality> getAllLocalities() {
+        return localityRepository.findAll();
+    }
+
+    @GetMapping("/Locality/{id}")
+    public Locality getLocalityById(@PathVariable Integer id) {
+        Locality locality =  localityRepository.findById(id).get();
+        return locality;
+    }
+
+    @PostMapping("/Locality")
+    public Locality createLocality(@RequestBody Locality Locality) {
+        return localityRepository.save(Locality);
+    }
+
+    @PutMapping("/Locality/{localityId}/{userId}{")
+    public Locality associate(@PathVariable Integer localityId, @PathVariable Integer userId) {
+
+        User user = userRepository.findById(userId).get();
+        Locality locality = localityRepository.findById(localityId).get();
+
+        locality.setUser(user);
+        user.getLocalities().add(locality);
+
+        userRepository.save(user);
+        localityRepository.save(locality);
+
+        return  locality;
+
+    }
+
+    @PutMapping("/Locality/{localityId}")
+    public Locality updateLocality(@PathVariable Integer localityId, @RequestBody Locality LocalityNew) {
+
+        Locality locality = localityRepository.findById(localityId).get();
+
+        locality.setName(LocalityNew.getName());
+        locality.setNeighborhood(LocalityNew.getNeighborhood());
+
+        localityRepository.save(locality);
+
+        return locality;
+    }
+
+
+    @DeleteMapping("/User/{localityId}")
+    public Locality deleteLocality(@PathVariable Integer localityId) {
+        Locality locality = localityRepository.findById(localityId).get();
+        userRepository.deleteById(localityId);
+        return locality;
+    }
+
+
+}
