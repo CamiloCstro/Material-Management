05f9eb3223acee781cd90513290f3427af7923f0
CamiloCstro <camilocstro@gmail.com>
2021-11-02 23:06:51 -0500

    Create rest controller to Registration model with post Mapping method

---
+++ src/main/java/com/materialmanagement/controller/RegistrationController.java
@@ -0,0 +1,24 @@
+package com.materialmanagement.controller;
+
+import com.materialmanagement.model.Registration;
+import com.materialmanagement.model.User;
+import com.materialmanagement.repository.IRegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/Registration")
+public class RegistrationController {
+
+    @Autowired
+    IRegistrationRepository iRegistrationRepository;
+
+    @PostMapping()
+    public Registration createRegistration(@RequestBody Registration registration) {
+        return iRegistrationRepository.save(registration);
+    }
+
+}
