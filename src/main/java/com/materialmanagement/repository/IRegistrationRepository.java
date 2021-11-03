bbee2e9e7c70f31da5fba84be22ee39e189b320b
CamiloCstro <camilocstro@gmail.com>
2021-11-02 23:03:15 -0500

    add Registration Jpa Repository

---
+++ src/main/java/com/materialmanagement/repository/IRegistrationRepository.java
@@ -0,0 +1,10 @@
+package com.materialmanagement.repository;
+
+import com.materialmanagement.model.Registration;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface IRegistrationRepository extends JpaRepository<Registration, Integer> {
+
+}
