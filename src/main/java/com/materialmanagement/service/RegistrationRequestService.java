b35729fb05a592951f6d9d57959ae52cac81894d
Camilo Castro <CamiloCstro@gmail.com>
2021-11-11 02:23:59 -0500

    Crate a method register User for register the users.

--- src/main/java/com/materialmanagement/service/RegistrationRequestService.java
+++ src/main/java/com/materialmanagement/service/RegistrationRequestService.java
@@ -1,12 +1,30 @@
 package com.materialmanagement.service;
 
 import com.materialmanagement.model.RegistrationRequest;
+import com.materialmanagement.model.User;
+import com.materialmanagement.model.UserRole;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
 @Service
+@AllArgsConstructor
 public class RegistrationRequestService {
 
+    private UserService userService;
+    private EmailValidatorService emailValidatorService;
+
     public String register(RegistrationRequest request){
-        return "okk nmelo";
+        boolean isValidEmail = emailValidatorService.test(request.getEmail());
+        if (!isValidEmail){
+            throw new IllegalStateException("Email no valido");
+        }
+       return userService.singUpUser(new User(
+               request.getFirstName(),
+               request.getLastName(),
+               request.getEmail(),
+               request.getPassword(),
+               UserRole.USER
+       ));
     }
 }
