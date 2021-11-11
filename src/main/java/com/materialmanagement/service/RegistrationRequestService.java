2752518908b9b058999ca26782d4fda0701fb4bc
Camilo Castro <CamiloCstro@gmail.com>
2021-11-10 20:31:18 -0500

    Create Service RegistrationRequestService with a method register.

---
+++ src/main/java/com/materialmanagement/service/RegistrationRequestService.java
@@ -0,0 +1,12 @@
+package com.materialmanagement.service;
+
+import com.materialmanagement.model.RegistrationRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RegistrationRequestService {
+
+    public String register(RegistrationRequest request){
+        return "okk nmelo";
+    }
+}
