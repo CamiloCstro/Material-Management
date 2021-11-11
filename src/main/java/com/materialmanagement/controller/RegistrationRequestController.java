3c285ea811e7f8eafc2d0eb9cb7fddd7e4a04695
Camilo Castro <CamiloCstro@gmail.com>
2021-11-10 20:26:32 -0500

    Create Controller to register a User

---
+++ src/main/java/com/materialmanagement/controller/RegistrationRequestController.java
@@ -0,0 +1,24 @@
+package com.materialmanagement.controller;
+
+
+import com.materialmanagement.model.RegistrationRequest;
+import com.materialmanagement.service.RegistrationRequestService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/registration")
+@AllArgsConstructor
+public class RegistrationRequestController {
+
+    private RegistrationRequestService registrationRequestService;
+
+    @PostMapping()
+    public String register(@RequestBody RegistrationRequest request){
+        return registrationRequestService.register(request);
+    }
+
+}
