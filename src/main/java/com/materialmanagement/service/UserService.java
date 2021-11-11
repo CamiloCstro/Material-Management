a66346592e41ca3d1ad801d995bf53167b812218
Camilo Castro <CamiloCstro@gmail.com>
2021-11-10 20:30:40 -0500

    Create a UserService implements userDetailsService and override the method loadUserByUsername

---
+++ src/main/java/com/materialmanagement/service/UserService.java
@@ -0,0 +1,22 @@
+package com.materialmanagement.service;
+
+import com.materialmanagement.repository.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private IUserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("No se encontro el email"));
+    }
+
+}
