595dd4d91307b218b1af4f2f5db143ffe613a599
Camilo Castro <CamiloCstro@gmail.com>
2021-11-10 20:23:09 -0500

    Create method findByEmail

--- src/main/java/com/materialmanagement/repository/IUserRepository.java
+++ src/main/java/com/materialmanagement/repository/IUserRepository.java
@@ -4,6 +4,11 @@ import com.materialmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByEmail(String email);
+
 }
