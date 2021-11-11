0a2dc367ecfc6b31f03da8e8dfc16cca9e43df7c
Camilo Castro <CamiloCstro@gmail.com>
2021-11-11 02:24:19 -0500

    Modify requestMapping path

--- src/main/java/com/materialmanagement/controller/RegistrationRequestController.java
+++ src/main/java/com/materialmanagement/controller/RegistrationRequestController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1/registration")
+@RequestMapping("api/v1/registrationUser")
 @AllArgsConstructor
 public class RegistrationRequestController {
 
